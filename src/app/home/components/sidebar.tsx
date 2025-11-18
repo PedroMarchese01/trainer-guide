96aaf7e833587cea9ee0f62de29192f2debc4c19
Pedro Marchese <rm563339@fiap.com.br>
2025-11-18 15:09:57 -0300

    Feat: deixar sidebar responsiva

--- src/app/home/components/sidebar.tsx
+++ src/app/home/components/sidebar.tsx
@@ -12,7 +12,8 @@ import {
   SidebarGroupContent,
   SidebarMenu,
   SidebarMenuItem,
-  SidebarMenuButton
+  SidebarMenuButton,
+  SidebarTrigger,
 } from "@/components/ui/sidebar";
 
 export default function Sidebar() {
@@ -20,29 +21,35 @@ export default function Sidebar() {
   const isActive = (path: string) => pathname === path;
 
   return (
-    <SidebarP 
+    <SidebarP
       variant="inset"
-      className="bg-black text-white border-r border-white/10"
+      className="bg-black text-white border-r border-white/10 h-full"
     >
-      <SidebarHeader className="bg-black text-white border-b border-white/10">
-        <h2 className="font-semibold text-lg">Trainee Guide</h2>
+      <SidebarHeader className="bg-black text-white border-b border-white/10 grid grid-cols-3">
+        <h2 className="font-semibold text-lg col-span-2 mx-auto">Trainee Guide</h2>
+        <SidebarTrigger className="text-white ml-auto"/>
       </SidebarHeader>
 
-      <SidebarContent className="bg-black text-white">
+      <SidebarContent className="bg-black text-white flex flex-col gap-4">
+
+        {/* DASHBOARD */}
         <SidebarGroup>
-          <SidebarGroupLabel className="text-gray-300 mb-4">
-            Navegação
+          <SidebarGroupLabel className="text-gray-300 mb-2">
+            Dashboard
           </SidebarGroupLabel>
 
           <SidebarGroupContent>
             <SidebarMenu>
 
-              <SidebarMenuItem className="mb-1">
+              <SidebarMenuItem>
                 <SidebarMenuButton asChild>
                   <Link
+                  prefetch
                     href="/home"
                     className={`block px-2 py-1 rounded ${
-                      isActive("/home") ? "bg-white text-black" : "text-white"
+                      isActive("/home")
+                        ? "bg-white text-black"
+                        : "text-white"
                     }`}
                   >
                     Home
@@ -50,12 +57,15 @@ export default function Sidebar() {
                 </SidebarMenuButton>
               </SidebarMenuItem>
 
-              <SidebarMenuItem className="mb-1">
+              <SidebarMenuItem>
                 <SidebarMenuButton asChild>
                   <Link
+                    prefetch
                     href="/home/profile"
                     className={`block px-2 py-1 rounded ${
-                      isActive("/home/profile") ? "bg-white text-black" : "text-white"
+                      isActive("/home/profile")
+                        ? "bg-white text-black"
+                        : "text-white"
                     }`}
                   >
                     Perfil
@@ -63,15 +73,18 @@ export default function Sidebar() {
                 </SidebarMenuButton>
               </SidebarMenuItem>
 
-              <SidebarMenuItem className="mb-1">
+              <SidebarMenuItem>
                 <SidebarMenuButton asChild>
                   <Link
-                    href="/home/guide"
+                    prefetch
+                    href="/home/ranking"
                     className={`block px-2 py-1 rounded ${
-                      isActive("/home/guide") ? "bg-white text-black" : "text-white"
+                      isActive("/home/ranking")
+                        ? "bg-white text-black"
+                        : "text-white"
                     }`}
                   >
-                    Guias
+                    Ranking
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
@@ -79,6 +92,95 @@ export default function Sidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+
+        {/* GUIAS E ESTUDOS */}
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-gray-300 mb-2">
+            Guias e Estudos
+          </SidebarGroupLabel>
+
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link
+                    prefetch
+                    href="/home/guide"
+                    className={`block px-2 py-1 rounded ${
+                      isActive("/home/guide")
+                        ? "bg-white text-black"
+                        : "text-white"
+                    }`}
+                  >
+                    Guias
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* COMUNIDADE */}
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-gray-300 mb-2">
+            Comunidade
+          </SidebarGroupLabel>
+
+          <SidebarGroupContent>
+            <SidebarMenu>
+
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link
+                    prefetch
+                    href="/home/answer"
+                    className={`block px-2 py-1 rounded ${
+                      isActive("/home/answer")
+                        ? "bg-white text-black"
+                        : "text-white"
+                    }`}
+                  >
+                    Responder Perguntas
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link
+                    prefetch
+                    href="/home/ask"
+                    className={`block px-2 py-1 rounded ${
+                      isActive("/home/ask")
+                        ? "bg-white text-black"
+                        : "text-white"
+                    }`}
+                  >
+                    Fazer Pergunta
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link
+                    prefetch
+                    href="/home/questions-admin"
+                    className={`block px-2 py-1 rounded ${
+                      isActive("/home/questions-admin")
+                        ? "bg-white text-black"
+                        : "text-white"
+                    }`}
+                  >
+                    Administração de Perguntas
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
       </SidebarContent>
 
       <SidebarFooter className="bg-black text-white border-t border-white/10">
--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,19 +1,28 @@
 "use client";
 
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger, Sidebar as SidebarP } from "@/components/ui/sidebar";
 import Sidebar from "./components/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="flex">
-        <aside>
-          <Sidebar />
-        </aside>
 
-        <div className="flex-1">
+      {/* Trigger no mobile */}
+      <div className=" p-2 bg-[#06030f]">
+        <SidebarTrigger className="text-white"/>
+      </div>
+
+      <div className="grid w-full h-full bg-[#080611] grid-cols-[auto_1fr]">
+        
+        {/* Container correto do shadcn */}
+        <SidebarP>
+          <Sidebar />
+        </SidebarP>
+
+        <main className="h-screen w-full">
           {children}
-        </div>
+        </main>
+
       </div>
     </SidebarProvider>
   );
